71620c06525b8bc9e772603e46bda0b428a607f1
vaishnaviboddupally <85918991+vaishnaviboddupally@users.noreply.github.com>
2024-08-03 20:44:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,9 +22,9 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events.js"));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
+//app.get("/*", (req, res) => {
+//  res.sendFile(path.join(__dirname + "/public/index.html"));
+//});
 
 // Listening PORT
 const port = process.env.PORT || 5000;
