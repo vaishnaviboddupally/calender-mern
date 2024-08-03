e82b9bc1c33228dd9ccc14760490a71adaca7c8c
vaishnaviboddupally <85918991+vaishnaviboddupally@users.noreply.github.com>
2024-08-03 19:49:47 +0530

    Update databaseValidators.js

--- server/helpers/databaseValidators.js
+++ server/helpers/databaseValidators.js
@@ -1,5 +1,5 @@
 const Event = require("../models/Event");
-const User = require("../models/User");
+const User = require("../models/user");
 
 const eventExistsById = async (req, res, next) => {
   const { id } = req.params;
